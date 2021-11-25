dc68c7c08d2ea60dd9b6921b66fd1d82497b8b32
mohamedfaroouk <mohamedfarouk15898@gmail.com>
2021-11-25 12:56:17 +0200

    last

---
+++ lib/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:okape/constants.dart';
+import 'package:okape/services/firebase.dart';
+
+class ChatScreen extends StatefulWidget {
+  ChatScreen({Key key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _user = types.User(id: token);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+          stream: getChat(token),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Something went wrong'));
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return Chat(
+              messages: snapshot.data.docs
+                  .map((e) => types.Message.fromJson(e.data()))
+                  .toList(),
+              onSendPressed: (PartialText) {
+                sendMessege(_user, PartialText.text, "admin");
+              },
+              user: _user,
+              showUserAvatars: true,
+              theme: DefaultChatTheme(),
+              showUserNames: true,
+            );
+          }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'dart:async';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 import 'package:connectivity/connectivity.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:okape/adminchattest.dart';
+import 'package:okape/chat_screen.dart';
+import 'package:okape/constants.dart';
+import 'package:okape/enter_name.dart';
 import 'package:okape/facebook.dart';
 import 'package:okape/no_network.dart';
+import 'package:okape/services/api.dart';
+import 'package:okape/services/firebase.dart';
+import 'package:okape/widgets.dart';
 import 'package:okape/youtube_webview.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Diohelp.init();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  token = await messaging.getToken();
+  userName = await getUserName(token);
+  await messaging.subscribeToTopic('users');
+
+  notifacationHandel();
+
+  FirebaseMessaging.onBackgroundMessage(firebaseonbackground);
+
+  print(userName);
+  print(token);
   runApp(MyApp());
 }
 
@@ -26,31 +51,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 // String selectedUrl = 'https://www.youtube.com/c/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9%D8%A7%D9%84%D8%B1%D8%B3%D9%85%D9%8A%D8%A9%D9%84%D9%84%D8%AD%D8%A7%D8%AC%D8%B9%D9%82%D8%A8%D9%8A';
 final Set<JavascriptChannel> jsChannels = [
   JavascriptChannel(
       name: 'Print',
-      onMessageReceived:
-          (JavascriptMessage message) {
+      onMessageReceived: (JavascriptMessage message) {
         print(message.message);
       }),
 ].toSet();
+
 class MyHomePage extends StatefulWidget {
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-  StreamSubscription<ConnectivityResult>
-  internetconnection;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  StreamSubscription<ConnectivityResult> internetconnection;
   bool isoffline = true;
-  final flutterWebviewPlugin =
-  new FlutterWebviewPlugin();
-  Widget ui = Scaffold() ;
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  Widget ui = Scaffold();
+  TabController tabController;
+  GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -64,8 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
           isoffline = true;
           ui = nonet(context);
         });
-      } else if (result ==
-          ConnectivityResult.wifi ||
+      } else if (result == ConnectivityResult.wifi ||
           result == ConnectivityResult.mobile) {
         //connection is from wifi
         setState(() {
@@ -74,19 +95,53 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       }
     });
-
-
-
-
+    tabController = TabController(vsync: this, length: 2);
     // TODO: implement initState
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return ui ;
+    return Scaffold(
+      key: scaffoldkey,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.mail,
+        ),
+        onPressed: () {
+          userName.isEmpty
+              ? scaffoldkey.currentState.showBottomSheet((context) {
+                  return EnterName();
+                })
+              : navto(ChatScreen(), context);
+        },
+      ),
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: TabBar(
+            controller: tabController,
+            padding: EdgeInsets.zero,
+            tabs: [Tab(text: "youtube"), Tab(text: "facebook")]),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                isoffline ? nonet(context) : chattest(),
+                isoffline ? nonet(context) : Container(color: Colors.white)
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: Center(child: Text("حط الاعلان هنا")),
+          )
+        ],
+      ),
+    );
     // return Scaffold(
     //   // appBar: AppBar(
     //   //
@@ -126,3 +181,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // );
   }
 }
+
+
+ /*   Container(
+                child: Column(
+                  children: [
+                    TextField(
+                      onSubmitted: (s) {
+                        createUser(s, token);
+                      },
+                    ),
+                    TextField(
+                      onSubmitted: (s) {
+                        sendMessege(types.User(id: "admin"), s, token);
+                      },
+                    ), */
